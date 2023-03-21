7a1da7fcedcc4d07a8f7994cfb14092879c2a5f0
Bogdan Mierloiu <bogdy.musiclover@gmail.com>
2023-03-21 10:43:33 +0200

    first commit

---
+++ src/main/java/com/bogdanmierloiu/CriminalRecords/CriminalRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.bogdanmierloiu.CriminalRecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriminalRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriminalRecordsApplication.class, args);
+	}
+
+}
